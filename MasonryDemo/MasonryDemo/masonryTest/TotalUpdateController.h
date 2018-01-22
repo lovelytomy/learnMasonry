21a0654de26d7e2ccd5b9ac7dbd5c9506020fef6
lovelytomy <535303865@qq.com>
2018-01-22 15:17:47 +0800

    modify

---
+++ MasonryDemo/MasonryDemo/masonryTest/TotalUpdateController.h
@@ -0,0 +1,13 @@
+//
+//  TotalUpdateController.h
+//  MasonryDemo
+//
+//  Created by tianqi on 2018/1/22.
+//  Copyright © 2018年 com.wiscess. All rights reserved.
+//
+
+#import "TQBaseViewController.h"
+
+@interface TotalUpdateController : TQBaseViewController
+
+@end
---
+++ MasonryDemo/MasonryDemo/masonryTest/TotalUpdateController.m
@@ -0,0 +1,37 @@
+//
+//  TotalUpdateController.m
+//  MasonryDemo
+//
+//  Created by tianqi on 2018/1/22.
+//  Copyright © 2018年 com.wiscess. All rights reserved.
+//
+
+#import "TotalUpdateController.h"
+
+@interface TotalUpdateController ()
+
+@end
+
+@implementation TotalUpdateController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
